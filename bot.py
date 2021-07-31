0a429fda0aecf40a087f4df2ec6497adcc490a1b
rostekus <rostyslavmosorov@gmail.com>
2021-07-31 18:38:11 +0200

    working bot

--- bot.py
+++ bot.py
@@ -1,19 +1,92 @@
 from bs4 import BeautifulSoup
 from requests import get
+import re
+from datetime import date, datetime
+import time;
 
-URL = 'https://www.vinted.pl/mezczyzni/ubrania/plaszcze-i-kurtki/kurtki/kurtki-puchowe/147380719-brazowa-kurtka-zimowa-meska-zara'
+BASE_URL = 'https://www.vinted.pl/ubrania?brand_id[]=9&catalog[]=1816&size_id[]=208&color_id[]=3'
+ts = int(time.time())
 
-page = get(URL)
+USER_BRANDS = ['zara']
 
-bs = BeautifulSoup(page.content, 'html.parser')
 
-bs = bs.find('div', class_ = 'row items-list')
-for offer in bs.find_all('a', class_ = 'item-box__brand'):
-    print
-    offer = str(offer)
-    for i in range(41,51):
-        if offer[i] == '"':
-            break
-    print(offer[40:i])
+def check_user(brands, prices, sizes):
+    number_of_right_element = 0
+    for brand in USER_BRANDS:
+        #print(sizes)
+        num_of_elements = brands.count(brand)
+        if num_of_elements >= 2:
+            for clothe, size in  zip(brands, sizes):
+                if clothe == brand and size == 'M':
+                    number_of_right_element += 1
+    return number_of_right_element
 
-    #for i in range(1,5):
+
+def get_links(URL):
+    page = get(URL)
+    bs = BeautifulSoup(page.content, 'html.parser')
+    soup = str(bs)
+    string = '"is_favourite":false,"is_hated":false},"url":"'
+    a = [m.start() for m in re.finditer(string, soup)]
+
+    links = []
+    for j in range(len(a)):
+        start = a[j] + 46
+        finish = 0
+        for i in range(start + 1, start + 1000):
+
+            # print(i)
+            if soup[i] == '"':
+                finish = i
+                break
+
+        links.append(soup[start:finish])
+    return links
+
+
+def get_arr(URL):
+    page = get(URL)
+    bs = BeautifulSoup(page.content, 'html.parser')
+    soup = bs.find('div', class_='row items-list')
+
+    prices = []
+    brands = []
+    sizes = []
+    for price, brand, size in zip(soup.find_all('div', class_='item-box__title'),
+                                  soup.find_all('a', class_='item-box__brand'),
+                                  soup.find_all('div', class_='u-ui-margin-right-small')):
+        size = size.get_text()
+
+        price = price.get_text().strip().split(',')[0]
+        brand = str(brand)
+        for i in range(41, 51):
+            if brand[i] == '"':
+                break
+
+        brand = brand[40:i]
+        prices.append(price)
+        brands.append(brand)
+        sizes.append(size)
+    # print(prices)
+    return brands, prices, sizes
+
+
+if __name__ == '__main__':
+    prices = []
+    brands = []
+    sizes = []
+    links = get_links(BASE_URL)
+    num_page = 1
+    while len(links) != 0:
+
+        URL = BASE_URL + '&time=' + str(ts) + '&page=' + str(num_page)
+
+        print(num_page)
+        links = get_links(URL)
+
+        for i, link in enumerate(links):
+            brands, prices, sizes = get_arr(link)
+
+            if check_user(brands, prices, sizes) != 0:
+                print(link)
+        num_page += 1
