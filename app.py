10cac1308a6e1ab24abdb31fcf3c358cc916e855
rostekus <rostyslavmosorov@gmail.com>
2021-08-02 21:30:06 +0200

    added argparse config file

--- app.py
+++ app.py
@@ -1,19 +1,57 @@
+from configparser import ConfigParser
 import bot
 import gmail
-from sys import argv
-
-
+import argparse
+from pathlib import Path
+# TODO:
+# save all possible brands from vinted site
 # check regex for adress
+# add ML 
+
 class App:
 
     def __init__(self):
-        pass
+        self.url, self.brands, self.size, self.price, self.num_of_clothes= self.setup()
+        #self.vinbot = bot.VintedBot(self.url, self.brands, self.size, self.price , self.num_of_clothes)
+        #while(True):
+            #self.vinbot.start()
+        
     
     def setup(self):
+        setup_file = Path('config.ini')
         
-        is_corrct = self.check_url(url)
-        if is_corrct:
-            return 
+        if not setup_file.is_file():
+
+            parser = argparse.ArgumentParser(description='Bot for searching clothes on vinted.com')
+            parser.add_argument('--url',required=True, type=str, nargs='?',
+                        help='Url for searching')
+            parser.add_argument('-b','--brands', type=str, nargs='?',
+                        help='An optional integer positional argument')
+            parser.add_argument('-s','--size', type=str, nargs='?',
+                        help='An optional integer positional argument')
+            parser.add_argument('-p','--price', type=int, nargs='?',
+                        help='An optional integer positional argument')
+            parser.add_argument('-n','--number', type=int, nargs='?',
+                        help='An optional integer positional argument')
+            args = parser.parse_args()
+            #check if are correctly written
+            brands = self.string_2_list(args.brands)
+            
+            print(args.url, brands , args.size, args.price, args.number)
+            return args.url, brands , args.size, args.price, args.number 
+        else:
+            parser = ConfigParser()
+            parser.read('config.ini')
+            print(parser.get('set', 'url'))
+            #check if are correctly written
+            brands = self.string_2_list(parser.get('set', 'brands'))
+            return parser.get('set', 'url'),brands,parser.get('set', 'size'),1,1
+
+    def string_2_list(self,text):
+        return list(text.lower().replace(' ','').split(','))
+
+
+    
 
     def check_url(self):
         pass
@@ -22,12 +60,9 @@ class App:
         pass
 
 
-BASE_URL = 'https://www.vinted.pl/ubrania?currency=PLN&search_id=471687498&status[]=6&status[]=1&status[]=2&size_id[]=208&order=price_low_to_high&brand_id[]=20682&brand_id[]=9&brand_id[]=123&brand_id[]=91&price_to=15'
-if __name__ == '__main__':
 
-    if(argv[1] == 'setup'):
-        BASE_URL = argv[2]
-    print(BASE_URL)
-    else:
-        vinbot = bot.VintedBot(BASE_URL, ['house'], 'M', 100, 1)
-        vinbot.start()
+
+if __name__ == '__main__':
+    app = App()
+
+        
