50ef53b5c91fe4610838be2465d84725ca225eb6
rostekus <rostyslavmosorov@gmail.com>
2021-08-02 13:41:17 +0200

    added app class

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ from sys import argv
 
 
 # check regex for adress
+class App:
+
+    def __init__(self):
+        pass
+    
+    def setup(self):
+        
+        is_corrct = self.check_url(url)
+        if is_corrct:
+            return 
+
+    def check_url(self):
+        pass
+
+    def send_email(self):
+        pass
 
 
 BASE_URL = 'https://www.vinted.pl/ubrania?currency=PLN&search_id=471687498&status[]=6&status[]=1&status[]=2&size_id[]=208&order=price_low_to_high&brand_id[]=20682&brand_id[]=9&brand_id[]=123&brand_id[]=91&price_to=15'
@@ -12,6 +28,6 @@ if __name__ == '__main__':
     if(argv[1] == 'setup'):
         BASE_URL = argv[2]
     print(BASE_URL)
-
-    #vinbot = bot.VintedBot(BASE_URL, ['house'], 'M', 100, 1)
-    #vinbot.start()
+    else:
+        vinbot = bot.VintedBot(BASE_URL, ['house'], 'M', 100, 1)
+        vinbot.start()
