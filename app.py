bea750f23305ad13093899f45061ab2fa1fd8018
rostekus <rostyslavmosorov@gmail.com>
2021-08-03 17:42:50 +0200

    added statistics function to create db

--- app.py
+++ app.py
@@ -3,19 +3,28 @@ import bot
 import gmail
 import argparse
 from pathlib import Path
+import json
+from string import ascii_uppercase
+import sqlite3
+from requests import get
+from bs4 import BeautifulSoup
+import re
+
+
 # TODO:
 # save all possible brands from vinted site
 # check regex for adress
-# add ML 
+# add ML ?
+#seaborn
 
 class App:
 
     def __init__(self):
-        self.url, self.brands, self.size, self.price, self.num_of_clothes= self.setup()
+        # self.url, self.brands, self.size, self.price, self.num_of_clothes= self.setup()
         #self.vinbot = bot.VintedBot(self.url, self.brands, self.size, self.price , self.num_of_clothes)
         #while(True):
             #self.vinbot.start()
-        
+        pass
     
     def setup(self):
         setup_file = Path('config.ini')
@@ -60,9 +69,68 @@ class App:
         pass
 
 
+    def urls_generator(self):
+        pattern = """<a class="follow__name" data-ch="br" href="\/brand\/(.+?)">
+                  (.+?)
+                 <\/a>
+                 <div class="follow__details">
+                  (.+?) p"""
+
+        alphabet = ascii_uppercase
+        base_url = 'https://www.vinted.pl/brands/by_letter/'
+        for letter in alphabet:
+            url = base_url + letter 
+            yield url
+    
+    
+    def all_brands_2_json(self):
+        pattern = """<a class="follow__name" data-ch="br" href="\/brand\/(.+?)">
+                  (.+?)
+                 <\/a>
+                 <div class="follow__details">
+                  (.+?) p"""
+        
+        for url in self.urls_generator():
+            page = get(url)
+            bs = BeautifulSoup(page.content, 'html.parser')
+            bs = bs.prettify()
+            data  = {}
+            data.update({letter :{text.group(2):text.group(3)for text in re.finditer(pattern, bs)}})
+            print(data)
+            with open('data.json', 'rw') as fp:
+                json.dump(data, fp)
+    
+    
+    def statistics_db(self):
+        # check if file exists
+        conn = sqlite3.connect('brands_statistics.db')
+        curr = conn.cursor()
+        
+        curr.execute("""
+        CREATE TABLE IF NOT EXISTS statistics (letter TEXT, brand TEXT, num INTEGER)""")
+       
+        pattern = """<a class="follow__name" data-ch="br" href="\/brand\/(.+?)">
+                  (.+?)
+                 <\/a>
+                 <div class="follow__details">
+                  (.+?) p"""
+        
+        
+        alphabet = ascii_uppercase
+        base_url = 'https://www.vinted.pl/brands/by_letter/'
+        for letter in alphabet:
+            url = base_url + letter 
+            print(url)
+            page = get(url)
+            bs = BeautifulSoup(page.content, 'html.parser')
+            bs = bs.prettify()
+            for match in re.finditer(pattern, bs):
+                curr.execute('INSERT INTO statistics VALUES (?, ?, ?)', (letter, match.group(2), match.group(3)))
+                conn.commit()
 
 
 if __name__ == '__main__':
     app = App()
+    app.statistics_db()
 
         
